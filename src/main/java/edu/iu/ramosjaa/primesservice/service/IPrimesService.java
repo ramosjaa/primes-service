e57d6f5e6a9d25912669be03082891ed13d9a493
Jack Ramos <ramos.jack525@yahoo.com>
2024-02-22 23:56:26 -0500

    initial commit

---
+++ src/main/java/edu/iu/ramosjaa/primesservice/service/IPrimesService.java
@@ -0,0 +1,5 @@
+package edu.iu.ramosjaa.primesservice.service;
+
+public interface IPrimesService {
+    boolean isPrime(long n);
+}
---
+++ src/test/java/edu/iu/ramosjaa/primesservice/service/PrimesServiceTest.java
@@ -0,0 +1,34 @@
+package edu.iu.ramosjaa.primesservice.service;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PrimesServiceTest {
+
+    PrimesService primesService = new PrimesService();
+
+    @Test
+    void _45IsNotPrime(){
+        int n = 45;
+        boolean expected = false;
+        boolean actual = primesService.isPrime(n);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void _539828945930573IsNotPrime(){
+        int n = 45;
+        boolean expected = false;
+        boolean actual = primesService.isPrime(n);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void _285191IsPrime(){
+        int n = 285191;
+        boolean expected = true;
+        boolean actual = primesService.isPrime(n);
+        assertEquals(expected, actual);
+    }
+}
