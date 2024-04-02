40de4e2722232f6e384b149f8c053b75916caa65
Jack Ramos <ramos.jack525@yahoo.com>
2024-04-02 19:11:07 -0400

    added cors to auth controller

--- src/main/java/edu/iu/ramosjaa/primesservice/controller/AuthenticationController.java
+++ src/main/java/edu/iu/ramosjaa/primesservice/controller/AuthenticationController.java
@@ -6,6 +6,7 @@ import edu.iu.ramosjaa.primesservice.service.TokenService;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.io.IOException;
 
 @RestController
+@CrossOrigin({"http://127.0.0.1:5500"})
 public class AuthenticationController {
     private AuthenticationManager authenticationManager;
     private final IAuthenticationService authenticationService;
