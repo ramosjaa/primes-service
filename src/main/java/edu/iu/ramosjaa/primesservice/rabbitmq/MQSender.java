7e0d06d6ad4705f9f7dbdd285774011ecc8a8405
Jack Ramos <ramos.jack525@yahoo.com>
2024-04-09 19:35:04 -0400

    practicum 13

---
+++ src/main/java/edu/iu/ramosjaa/primesservice/rabbitmq/MQSender.java
@@ -0,0 +1,33 @@
+package edu.iu.ramosjaa.primesservice.rabbitmq;
+
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+
+import java.text.MessageFormat;
+
+public class MQSender {
+    private final Queue queue;
+    private final RabbitTemplate rabbitTemplate;
+
+    public Queue getQueue() {
+        return queue;
+    }
+
+    public RabbitTemplate getRabbitTemplate() {
+        return rabbitTemplate;
+    }
+
+    public MQSender(Queue queue, RabbitTemplate rabbitTemplate){
+        this.queue = queue;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    public void sendMessage(String username, int n, boolean isPrime){
+        String message = MessageFormat.format(
+                "customer:{0}, n: {1}, isPrime: {2}", username, String.valueOf(n), isPrime
+        );
+
+        message = "{" + message + "}";
+        rabbitTemplate.convertAndSend("primes", message);
+    }
+}
